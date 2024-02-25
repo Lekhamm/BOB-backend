373e09298a90007a9647a0c90c725ce0e914851c
lekha <mmlekha2k@gmail.com>
2024-02-25 13:12:55 +0530

    first commit

---
+++ sociopaedia2/src/main/java/com/zos/sociopaedia2/Sociopaedia2Application.java
@@ -0,0 +1,13 @@
+package com.zos.sociopaedia2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sociopaedia2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sociopaedia2Application.class, args);
+	}
+
+}
---
+++ sociopaedia2/src/test/java/com/zos/sociopaedia2/Sociopaedia2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zos.sociopaedia2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sociopaedia2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
